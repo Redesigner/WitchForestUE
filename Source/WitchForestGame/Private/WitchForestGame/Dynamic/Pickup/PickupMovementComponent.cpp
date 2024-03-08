948ec50643bc7a014be461f06f17fc55368258dc
steph <stephenwmelnick@gmail.com>
2024-03-08 17:29:58 -0500

    Custom PickupMovementComponent

--- Source/WitchForestGame/Private/WitchForestGame/Dynamic/Pickup/PickupMovementComponent.cpp
+++ Source/WitchForestGame/Private/WitchForestGame/Dynamic/Pickup/PickupMovementComponent.cpp
@@ -3,3 +3,115 @@
 
 #include "WitchForestGame/Dynamic/Pickup/PickupMovementComponent.h"
 
+// Copyright (c) 2024 Stephen Melnick
+
+
+#include "WitchForestGame/Dynamic/Pickup/PickupMovementComponent.h"
+
+#include "Net/UnrealNetwork.h"
+
+
+UPickupMovementComponent::UPickupMovementComponent()
+{
+	SetIsReplicatedByDefault(true);
+}
+
+
+void UPickupMovementComponent::OnRep_PickupMovement()
+{
+	if (!UpdatedComponent)
+	{
+		return;
+	}
+
+	//UE_LOG(LogTemp, Display, TEXT("Replicated pickup movement."));
+
+	UpdatedComponent->ComponentVelocity = PreviousMovement.Velocity;
+
+	FVector Delta = PreviousMovement.Location - UpdatedComponent->GetComponentLocation();
+	DrawDebugDirectionalArrow(GetWorld(), PreviousMovement.Location, PreviousMovement.Location + PreviousMovement.Velocity, 10.0f, FColor::Red, false, 0.5f);
+
+	UpdatedComponent->MoveComponent(Delta, UpdatedComponent->GetComponentRotation(), false);
+}
+
+/*
+void UPickupMovementComponent::OnRep_PreviousLocation()
+{
+	if (!UpdatedComponent)
+	{
+		return;
+	}
+	// DrawDebugSphere(GetWorld(), UpdatedComponent->GetComponentLocation(), 50.0f, 16, FColor::Red, false, 1.0f);
+	FVector Delta = PreviousLocation - UpdatedComponent->GetComponentLocation();
+	UpdatedComponent->MoveComponent(Delta, UpdatedComponent->GetComponentRotation(), false);
+}
+*/
+
+void UPickupMovementComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (!UpdatedComponent)
+	{
+		return;
+	}
+
+	if (!bActiveMovement)
+	{
+		return;
+	}
+
+	FVector NewVelocity = UpdatedComponent->GetComponentVelocity();
+
+	if (!bOnFloor)
+	{
+		NewVelocity += FVector(0.0f, 0.0f, GetWorld()->GetGravityZ() * DeltaTime);
+	}
+	else
+	{
+		// Apply friction?
+	}
+
+	const FVector Delta = DeltaTime * NewVelocity;
+
+	FHitResult HitResult;
+	SafeMoveUpdatedComponent(Delta, UpdatedComponent->GetComponentRotation(), true, HitResult);
+
+	if (HitResult.IsValidBlockingHit())
+	{
+		const FVector AbsorbedVelocity = HitResult.Normal.Dot(NewVelocity) * HitResult.Normal;
+		NewVelocity -= bBounce ? AbsorbedVelocity * (1.0f + BounceRatio) : AbsorbedVelocity;
+
+		if (NewVelocity.Z <= 10.0f && HitResult.Normal.Z >= 0.95f)
+		{
+			NewVelocity.Z = 0.0f;
+			// bOnFloor = true;
+		}
+	}
+
+	UpdatedComponent->ComponentVelocity = NewVelocity;
+	FPickupReplicatedMovement NewMovement;
+	NewMovement.Velocity = NewVelocity;
+	NewMovement.Location = UpdatedComponent->GetComponentLocation();
+	PreviousMovement = NewMovement;
+
+	// PreviousLocation = UpdatedComponent->GetComponentLocation();
+}
+
+void UPickupMovementComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION(ThisClass, PreviousMovement, COND_SkipOwner);
+	// DOREPLIFETIME_CONDITION(ThisClass, PreviousLocation, COND_SkipOwner);
+}
+
+void UPickupMovementComponent::SetVelocity(FVector InVelocity)
+{
+	if (!UpdatedComponent)
+	{
+		return;
+	}
+
+	UpdatedComponent->ComponentVelocity = InVelocity;
+}
--- Source/WitchForestGame/Public/WitchForestGame/Dynamic/Pickup/PickupMovementComponent.h
+++ Source/WitchForestGame/Public/WitchForestGame/Dynamic/Pickup/PickupMovementComponent.h
@@ -6,12 +6,58 @@
 #include "GameFramework/MovementComponent.h"
 #include "PickupMovementComponent.generated.h"
 
-/**
- * 
- */
+
+USTRUCT(BlueprintType)
+struct FPickupReplicatedMovement
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FVector Location;
+
+	UPROPERTY()
+	FVector Velocity;
+};
+
 UCLASS()
 class WITCHFORESTGAME_API UPickupMovementComponent : public UMovementComponent
 {
 	GENERATED_BODY()
-	
+
+
+	UPROPERTY(ReplicatedUsing = OnRep_PickupMovement)
+	FPickupReplicatedMovement PreviousMovement;
+
+	// UPROPERTY(ReplicatedUsing = OnRep_PreviousLocation)
+	// FVector PreviousLocation;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Movement, meta = (AllowPrivateAccess = true))
+	bool bBounce = true;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Movement, meta = (AllowPrivateAccess = true, ClampMin = 0.0f, ClampMax = 0.0f))
+	float BounceRatio = 0.5f;
+
+	bool bOnFloor = false;
+
+	bool bActiveMovement = true;
+
+
+
+	UFUNCTION()
+	void OnRep_PickupMovement();
+
+	// UFUNCTION()
+	// void OnRep_PreviousLocation();
+
+
+	void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+public:
+	UPickupMovementComponent();
+
+	void SetActiveMovement(bool bInActive) { bActiveMovement = bInActive; }
+
+	void SetVelocity(FVector InVelocity);
 };
